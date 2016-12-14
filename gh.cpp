cc2f45aa2e0c3c8b9b41190dbefc8eb1e65925c1
IZCHND066 <IZCHND066@IZCHND066-PC>
2016-12-14 13:58:46 +0530

    CPP

---
+++ gh.cpp
@@ -0,0 +1,18 @@
+/*
+ * gh.cpp
+ *
+ *  Created on: Nov 28, 2016
+ *      Author: IZCHND066
+ */
+
+#include "gh.h"
+
+gh::gh() {
+	// TODO Auto-generated constructor stub
+
+}
+
+gh::~gh() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ gh.h
@@ -0,0 +1,17 @@
+/*
+ * gh.h
+ *
+ *  Created on: Nov 28, 2016
+ *      Author: IZCHND066
+ */
+
+#ifndef GH_H_
+#define GH_H_
+
+class gh {
+public:
+	gh();
+	virtual ~gh();
+};
+
+#endif /* GH_H_ */
